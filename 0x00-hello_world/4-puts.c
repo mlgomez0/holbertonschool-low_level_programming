9f075e6cf7a958d91eb4bda374b896d2e592fb83
mlgomez0 <mlgomez230@hotmail.com>
2019-09-18 22:11:11 +0000

    First Commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: string
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
