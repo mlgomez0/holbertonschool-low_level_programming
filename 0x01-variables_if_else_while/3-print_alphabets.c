c9532cf43122787a718149ecff8c3a5adbabf0a9
mlgomez0 <mlgomez230@hotmail.com>
2019-09-19 22:06:38 +0000

    First commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: integer value
+  */
+int main(void)
+{
+	char m;
+	char l;
+
+	for (m = 'a'; m <= 'z'; m++)
+		putchar(m);
+	for (l = 'A'; l <= 'Z'; m++)
+		putchar(l);
+
+	putchar('\n');
+
+	return (0);
+}
