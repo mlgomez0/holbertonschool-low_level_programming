11abf4e8752bbacdb69621aeb2980f693de7790c
mlgomez0 <mlgomez230@hotmail.com>
2019-09-19 23:01:36 +0000

    First commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: integer value
+  */
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+
+	putchar('\n');
+
+	return (0);
+}
