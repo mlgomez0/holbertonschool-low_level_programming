8b79a388e8d32068715a19b8308dde6a40c36305
mlgomez0 <mlgomez230@hotmail.com>
2019-09-20 02:15:32 +0000

    First commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: integer value
+  */
+int main(void)
+{
+	int m;
+
+	m = '0';
+	while (m <= '9')
+	{
+		putchar(m);
+		if (m != '9')
+			putchar(44);
+		m++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
