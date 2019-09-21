cc75f4a8ab6413ef49ad62df28c0a37d6e5936ad
mlgomez0 <mlgomez230@hotmail.com>
2019-09-21 02:47:07 +0000

    First commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: integer value
+  */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (!(i == '8' && j == '9'))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
