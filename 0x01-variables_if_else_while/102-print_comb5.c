fc6109585dc9dcf1c77c29cb4ba7a865365d4d6c
mlgomez0 <mlgomez230@hotmail.com>
2019-09-21 04:40:37 +0000

    First commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: integer value
+  */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+
+			for (k = i; k <= '9'; k++)
+			{
+				for (m = j + 1; m <= '9'; m++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+					if (!(i == '9' && j == '8' && k == '9' && m == '9'))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
