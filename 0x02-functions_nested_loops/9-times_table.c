5cd7f6b9bce1efb51ee9807b123ded076c41a7b4
mlgomez0 <mlgomez230@hotmail.com>
2019-09-24 16:15:54 +0000

    Fourth commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,25 +14,26 @@ void times_table(void)
 			l = n * m;
 			if (l < 10)
 			{
-				_putchar('0' + l);
-				if (m != 9)
+				if (m != 0)
 				{
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
 				}
+				_putchar('0' + l);
 			}
 			else
 			{
 				o = l / 10;
 				p = l % 10;
-				_putchar('0' + o);
-				_putchar('0' + p);
-				if (m != 9)
+
+				if (m != 0)
 				{
 					_putchar(44);
 					_putchar(32);
 				}
+				_putchar('0' + o);
+				_putchar('0' + p);
 			}
 		}
 		_putchar('\n');
