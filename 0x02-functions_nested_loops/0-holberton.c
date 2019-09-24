623ee827653a0988fdd98394026adc41a796d635
mlgomez0 <mlgomez230@hotmail.com>
2019-09-24 20:14:24 +0000

    fifth commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,12 @@
 #include"holberton.h"
 /**
- *Main-Prints an string
+ *main-Prints an string
  *
- *Return- integer zero
+ *Return: integer zero
  */
 int main(void)
 {
-	char str[] ={'H','o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char str[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	int i;
 
 	for (i = 0; i <= 8; i++)
