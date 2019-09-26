d252b6b0492e5aeb5fe41ef54a08a3e5ce28a2fa
mlgomez0 <mlgomez230@hotmail.com>
2019-09-26 16:19:41 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *mul - determine if a given integer is a digit
+ *@a:parameter to be evaluated
+ *@b:parameter to be evaluated
+ *Return:product of multiplication of two integers
+ */
+
+int mul(int a, int b)
+{
+	int l;
+
+	l = a * b;
+	return (l);
+}
