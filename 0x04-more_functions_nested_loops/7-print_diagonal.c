3b3ff793dde12f607a4224e3df3a28d2b6eb78f2
mlgomez0 <mlgomez230@hotmail.com>
2019-09-26 21:15:56 +0000

    First commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,9 +15,9 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 1; j < i; j++)
 			{
-				_putchar(' ');
+				_putchar(32);
 			}
 			_putchar(92);
 			_putchar('\n');
