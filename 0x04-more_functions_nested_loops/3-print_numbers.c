fe898ce69d46817252f3d20129b7d4a5ee9de5a3
mlgomez0 <mlgomez230@hotmail.com>
2019-09-26 18:13:30 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_number - determine if a given integer is a digit
+ *
+ *Return:print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
