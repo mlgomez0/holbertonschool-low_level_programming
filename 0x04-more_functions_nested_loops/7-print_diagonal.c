b5fab4cf7a583b6b60765fbff3c57fec8f07e001
mlgomez0 <mlgomez230@hotmail.com>
2019-09-26 20:34:52 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *print_diagonal - prints a diagonal
+ *@n:number of times the \ caracter should be printed
+ *Return:a diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
