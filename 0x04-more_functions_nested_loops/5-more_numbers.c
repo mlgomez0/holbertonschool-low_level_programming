4c580387871b962378b5be9db8a0b31cc0f37133
mlgomez0 <mlgomez230@hotmail.com>
2019-09-26 18:48:52 +0000

    First commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ *more_numbers - prints 10 times the tumbers from 0 to 14
+ *
+ *Return:print a table with 10 lines
+ */
+
+void more_numbers(void)
+{
+	int i, j, l, m;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j <= 9)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				l = j / 10;
+				m = j % 10;
+				_putchar('0' + l);
+				_putchar('0' + m);
+			}
+		}
+		_putchar('\n');
+	}
+}
