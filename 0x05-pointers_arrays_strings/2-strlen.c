189a6e1273eae240ed3276f9f9a9181b53f05e59
mlgomez0 <mlgomez230@hotmail.com>
2019-09-30 22:03:05 +0000

    First Commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_strlen - determines the lenght of an string
+ *@s:string to be evaluated
+ *Return: An integer with the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
