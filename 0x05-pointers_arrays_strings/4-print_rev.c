9e23f9ecf10fbd162bd1043dd5c9d5fa4aa86bda
mlgomez0 <mlgomez230@hotmail.com>
2019-10-01 02:35:13 +0000

    Second Commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_rev - prints a given string in reverse order
+ *@s:string to be evaluated
+ *Return:An string of character
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		 i++;
+	}
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
