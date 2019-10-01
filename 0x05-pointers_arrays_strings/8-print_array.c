d3c7fecacfd27db321c2a4a33401b36160ad3fc6
mlgomez0 <mlgomez230@hotmail.com>
2019-10-01 22:05:49 +0000

    Third commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 	{
 		if (i != (n - 1))
 		{
-			printf("%d, ", a[i]);
+			printf("%i, ", a[i]);
 		}
 		else
 		{
-			printf("%d\n", a[i]);
+			printf("%i\n", a[i]);
 		}
 	}
 }
