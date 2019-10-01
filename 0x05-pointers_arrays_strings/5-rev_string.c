04396696cc873dfb92285c47868eee2ea6e19bd9
mlgomez0 <mlgomez230@hotmail.com>
2019-10-01 04:31:12 +0000

    Second Commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *rev_string - modifies a given string in reverse order
+ *@s:string to be evaluated
+ *Return:An string of character
+ */
+void rev_string(char *s)
+{
+	int i, j, m, l, max;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	max = i - 1;
+	for (j = max; j >= 0; j--)
+	{
+		l = max - j;
+		m = s[j];
+		s[j] = s[l];
+		s[l] = m;
+	}
+}
