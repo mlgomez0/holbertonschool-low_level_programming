c902e8a37b0257e03601b20ffda75d8c2832128e
mlgomez0 <mlgomez230@hotmail.com>
2019-10-04 03:41:37 +0000

    Second commit

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ *rot13 - encodes a string into rot13
+ *@s:array to review
+ *Return: char
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char inp[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+		'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+		'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z'};
+	char out[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'a', 'b',
+		'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[i] == inp[j])
+			{
+				s[i] = out[j];
+			}
+		}
+	}
+	return (s);
+}
