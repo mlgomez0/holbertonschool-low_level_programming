50ded351f8da1b472200ff606f6aa30dffc6064f
mlgomez0 <mlgomez230@hotmail.com>
2019-10-06 21:22:51 +0000

    sixth commit

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,15 +8,21 @@
 
 void print_number(int n)
 {
+	unsigned int m;
+
 	if (n < 0)
 	{
-		n = n * -1;
+		m = n * -1;
 		_putchar('-');
 	}
-	if (n / 10)
+	else
+	{
+		m = n;
+	}
+	if (m / 10)
 
-		print_number(n / 10);
+		print_number(m / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(m % 10 + '0');
 
 }
