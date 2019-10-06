64882564f5718df55f88779c505efe98d64770a8
mlgomez0 <mlgomez230@hotmail.com>
2019-10-06 20:39:43 +0000

    sixth commit

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include<stdlib.h>
 
 /**
  *print_number - print every number using _putchar function (by char)
@@ -8,6 +9,9 @@
 
 void print_number(int n)
 {
+	int * p = (int *) malloc(2 * sizeof(int));
+	*p = n;
+
 	if (n < 0)
 	{
 		n = n * -1;
@@ -18,4 +22,6 @@ void print_number(int n)
 		print_number(n / 10);
 
 	_putchar(n % 10 + '0');
+
+	free(p);
 }
