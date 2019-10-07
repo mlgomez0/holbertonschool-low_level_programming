733ee52e0e43157ada14f502a84359a40c77ad62
mlgomez0 <mlgomez230@hotmail.com>
2019-10-07 22:41:57 +0000

    Firts commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include"holberton.h"
+
+/**
+ *_strstr - compare one string to other to find a substring
+ *@haystack:string to be scanned
+ *@needle: accept: string to compare with
+ *Return:address of first coincidence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (needle[i] != '\0')
+	{
+		i++
+	{
+	n = i - 1;
+	while (haystack[j] != '\0')
+		if (haystack[j] == needle[j])
+		{
+			a++;
+		}
+			j++;
+		}
+		j = 0;
+	i++;
+	}
+	return ('\0');
+}
