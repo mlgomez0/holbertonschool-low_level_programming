0e9e9af46291a462cc1e8e6186e60461cff3f132
Holberton <holberton@Holbertons-iMac-3.local>
2019-10-12 14:42:55 -0500

    Second Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - say the name
+ *@argc: not used
+ *@argv: array
+ *Return : an integer
+ */
+int main(__attribute__((unused)) int argc, argv)
+{
+	printf("%s\n", argv[0]);
+}
