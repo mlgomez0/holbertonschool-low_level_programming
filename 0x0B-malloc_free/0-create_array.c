508cc9e436f7261cfe39cf7a6ba70275e59f3bed
mlgomez0 <mlgomez230@hotmail.com>
2019-10-15 18:30:41 +0000

    First Commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include"holberton.h"
+#include<stdlib.h>
+
+/**
+ *create_array - creates an array of specified size and initialized with specified char
+ *@size: size of the array
+ *@c: char to initiate the array
+ *return:a pointer to the array
+ */
+
+ char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *t;
+
+	i = 0;
+	t = (char *) malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	if (t == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		t[i] = c;
+	}
+	return (t);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# this folder has examples to apply dynamic memory allocation with malloc and free
