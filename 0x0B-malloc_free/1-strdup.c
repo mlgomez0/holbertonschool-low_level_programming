bfe2534d74dd5e7e889988f2c8daaa4c91691950
mlgomez0 <mlgomez230@hotmail.com>
2019-10-15 19:15:17 +0000

    First Commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include"holberton.h"
+#include<stdlib.h>
+
+/**
+ *_strdup - creates a copy of a given array
+ *@str: array to be copied
+ *Return:a pointer to the array
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int j;
+	char *t;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+
+	if (i == 0)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+	i = i - 1;
+	t = (char *) malloc(i * sizeof(char));
+	if (t == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+	{
+		t[j] = str[j];
+	}
+	return (t);
+}
