e4438b70586504eaef16f667f924907662dc1d40
mlgomez0 <mlgomez230@hotmail.com>
2019-10-17 19:46:16 +0000

    First commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
-	p = malloc(sizeof(char) * (i + j));
+
+	p = malloc(sizeof(char) * (i + j + 1));
 		if (p == NULL)
 			return (NULL);
 
