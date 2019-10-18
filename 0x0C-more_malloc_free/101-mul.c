c6bf611d5e228bc51f1f4ebc0f46ca8eff378864
mlgomez0 <mlgomez230@hotmail.com>
2019-10-18 02:59:37 +0000

    Second commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,68 @@
+#include"holberton.h"
+#include<stdlib.h>
+
+/**
+ *main - multiply two numbers
+ *@argc:number of elements to evaluate
+ *@argv:array of positive integers
+ *Return: an integer
+ */
+int main(argc, **argv)
+{
+	l = 1;
+	char err[5] = {'E', 'r', 'r', 'o', 'r')
+	if (argc != 3)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			_putchar(err[i]);
+		}
+		_putchar('\n');
+		exit(98);
+	}
+	i = 0;
+
+	for (j = 1; j < argc; j++)
+	{
+		for (k = 0; argv[j][k]; k++)
+		{
+			l++;
+			if (argv[j][k] < '0' || argv[j][k] > '9')
+			{
+				for (i = 0; i < 5; i++)
+				{
+					_putchar(err[i]);
+				}
+				_putchar('\n');
+				exit(98);
+			}
+		}
+	}
+	multi_num(l, argc, **argv);
+	return (0)
+}
+/**
+ *multi_num - multiply two numbers
+ *@c:number of main arguments
+ *@p:pointer of pointer
+ *Return: new size of the array
+  *Return:A pointer to the string with new space in memory
+ */
+
+char  *multi_num(int l, int argc, char **argv)
+{
+	char *p;
+
+
+	p = malloc((2 * l * sizeof(char)) + 1);
+		if (p == NULL)
+			return (NULL);
+
+	for (i = 1; i < argc; i++)
+	{
+
+		p[i] = ((char *)ptr)[i];
+	}
+	free(ptr);
+	return (p);
+}
