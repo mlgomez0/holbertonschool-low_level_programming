535d9e97f2edce20476ad1600bc8c1db11aff850
mlgomez0 <mlgomez230@hotmail.com>
2019-10-21 20:56:59 +0000

    First Commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,17 +8,17 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
 
-	}
 	if (d != NULL)
 	{
 		if ((*d).name == NULL)
 			printf("Name: (nil)\n");
-		else if ((*d).owner == NULL)
+		else
+			printf("Name: %s\n",  (*d).name);
+		printf("Age: %f\n", (*d).age);
+		if ((*d).owner == NULL)
 			printf("Owner: (nil)\n");
 		else
-			printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+			printf("Owner: %s\n", (*d).owner);
 	}
 }
