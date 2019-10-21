5f22e5b760d0704b00503ab8eb09270cc9af2b7c
mlgomez0 <mlgomez230@hotmail.com>
2019-10-21 19:38:52 +0000

    First Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ *struct dog - information for dogs
+ *@name: First member
+ *@age: Second member
+ *@owner: Third member
+ *Description: structure to with dog information
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
