c57f9446f566c6f09f4a42cdf460666fe2663517
mlgomez0 <mlgomez230@hotmail.com>
2019-10-26 19:30:06 +0000

    third commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,21 +2,24 @@
 #define VARIADIC
 #include<stdio.h>
 #include<stdarg.h>
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+
 /**
-* struct type - Struct type
-*
-* @let: char to be evaluated
-* @fun: The function associated
-*/
+ * struct type - Struct type
+ *
+ * @let: char to be evaluated
+ * @fun: The function associated
+ */
+
 
 typedef struct type
 {
 	char let;
 	void (*fun)(va_list);
 } select;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
