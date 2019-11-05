89302e210ec411292489a4ffe14bf4fc38ec91fe
mlgomez0 <mlgomez230@hotmail.com>
2019-11-05 19:52:40 +0000

    Third correction of check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,12 +14,10 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
-	listint_t *tmp, *tmpd, *tmpa, *new_node;
+	listint_t *tmp, *tmpd, *new_node;
 	unsigned int i;
 
 	i = 0;
-	if (head == NULL || *head == NULL)
-		return (NULL);
 	tmp = *head;
 	new_node = (listint_t *) malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -27,26 +25,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	while (i != idx && tmp != NULL)
-	{
-		tmpa = tmp;
-		tmpd = tmp->next;
-		tmp = tmpd;
-		i++;
-	}
-	if (i == idx && i == 0)
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	else if (i == idx)
+	while (i != (idx - 1))
 	{
-		tmpd = tmpa->next;
-		tmpa->next = new_node;
-		new_node->next = tmpd;
-		return (new_node);
+		if (tmp == NULL)
+			return (NULL);
+		tmpd = tmp->next;
+		tmp = tmpd;
+		i++;
 	}
-
-	return (NULL);
+	tmpd = tmp->next;
+	tmp->next = new_node;
+	new_node->next = tmpd;
+	return (new_node);
 }
