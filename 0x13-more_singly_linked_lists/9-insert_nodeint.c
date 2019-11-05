b986d4eacdc004e76b4a628268ffd92a45660b7d
mlgomez0 <mlgomez230@hotmail.com>
2019-11-05 18:01:47 +0000

    Null condition for head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	i = 0;
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	tmp = *head;
 	new_node = (listint_t *) malloc(sizeof(listint_t));
