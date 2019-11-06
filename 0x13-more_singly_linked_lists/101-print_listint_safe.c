aac66a292fce3f39a08bf999b543c069bd8bd9c1
mlgomez0 <mlgomez230@hotmail.com>
2019-11-06 01:48:14 +0000

    101-advance

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,6 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			return (i + 1);
-			exit(98);
 		}
 		head = head->next;
 		i++;
