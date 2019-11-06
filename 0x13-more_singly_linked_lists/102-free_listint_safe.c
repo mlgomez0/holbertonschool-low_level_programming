8d35e21becf586c5ba08a48e64c8cbcaee0f33c7
mlgomez0 <mlgomez230@hotmail.com>
2019-11-06 02:50:21 +0000

    102-advanced

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *free_listint_safe - free the memory space in a linked list
+ *@h: head of the linked list
+ *Return: returns the number of nodes
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+
+	listint_t *tmp;
+	int i;
+
+	i = 0;
+	if (h == NULL)
+		return (0);
+	while (*h != NULL)
+	{
+		if ((*h)->next > *h)
+		{
+			h = NULL;
+			return (i + 1);
+		}
+		tmp = (*h)->next;
+		free(*h);
+		*h  = tmp;
+		i++;
+	}
+	h = NULL;
+	return (i);
+}
