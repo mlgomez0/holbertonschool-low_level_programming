5d461e15b40f499c6b140e44a9b71725c4d8d997
mlgomez0 <mlgomez230@hotmail.com>
2019-11-07 23:32:30 +0000

    Example # 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "holberton.h"
+
+/**
+ *get_bit - get the bit in a given index
+ *@n:unsigned int to be evaluated
+ *@index:index to evalute
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (n)
+	{
+		if (i == index)
+			return (n % 2);
+
+		n = n / 2;
+		i++;
+	}
+	return (-1);
+}
