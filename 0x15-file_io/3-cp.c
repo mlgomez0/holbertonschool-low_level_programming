fc836568229820e2421b507edffdd4a076238f1f
mlgomez0 <mlgomez230@hotmail.com>
2019-11-12 23:53:16 +0000

    Example #3, v3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,7 @@ int main(int ac, char **av)
  */
 int copy_file(const char *file_from, const char *file_to)
 {
-	int fdf, fdt;
+	int fdf, fdt, m = 0;
 	ssize_t whatwrote, whatread;
 	char *buf[1024];
 
@@ -44,13 +44,9 @@ int copy_file(const char *file_from, const char *file_to)
 		exit(98);
 	}
 	fdt = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (fdt == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
 	while ((whatread = read(fdf, buf, 1024)) > 0)
 	{
+		m = 1;
 		whatwrote = write(fdt, buf, whatread);
 		if (whatwrote == -1)
 		{
@@ -58,7 +54,11 @@ int copy_file(const char *file_from, const char *file_to)
 			exit(99);
 		}
 	}
-
+	if (m == 0 || fdt == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
 	if (close(fdf) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fdf);
