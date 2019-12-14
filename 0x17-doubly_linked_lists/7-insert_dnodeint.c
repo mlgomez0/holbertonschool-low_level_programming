e69d4f06a19dc7fe7a718903a90b049d4847bc0e
mlgomez0 <mlgomez230@hotmail.com>
2019-12-14 21:08:30 +0000

    Code refactorizing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,49 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *newnode;
-	unsigned int i;
+	unsigned int i = 0, len;
 
-	if (h == NULL  || (*h == NULL && idx != 0))
+	if (h == NULL || (*h == NULL && idx > 0))
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	len = dlistint_len1(*h);
+	if (idx > len)
+		return (NULL);
+	if (idx == len)
+		return (add_dnodeint_end(h, n));
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
 	newnode->prev = NULL;
-	if (*h == NULL)
-		*h = newnode;
-	if (idx == 0)
+	while (idx != i)
 	{
-		newnode->next = temp;
-		temp->prev = newnode;
-		*h = newnode;
-		return (newnode);
-	}
-	for (i = 0; i != idx; i++)
-	{
-		if (temp->next == NULL)
-		{
-			if (i == (idx - 1))
-			{
-				temp->next = newnode;
-				newnode->prev = temp;
-				return (newnode);
-			}
-			else
-				return (NULL);
-		}
 		temp = temp->next;
+		i++;
 	}
 	temp->prev->next = newnode;
 	newnode->prev = temp->prev;
 	temp->prev = newnode;
 	newnode->next = temp;
 	return (newnode);
+}
+/**
+ *dlistint_len1 - determine the number of elements of a doubly linked list
+ *@h: head of the list to be printed
+ *Return: the number of nodes in the list
+ */
+unsigned int dlistint_len1(const dlistint_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -30,5 +30,6 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void print_inv(const dlistint_t *h);
+unsigned int dlistint_len1(const dlistint_t *h);
 
 #endif
