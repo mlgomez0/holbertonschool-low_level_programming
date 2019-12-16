4a71e07443acc05ebc4ded45d37f97b631fb0bf3
mlgomez0 <mlgomez230@hotmail.com>
2019-12-16 22:49:25 +0000

    Second commit LD-PRELOAD

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 wget -P .. https://github.com/mlgomez0/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/Giga_million/libmirror.so
-LD_PRELOAD=../libmirror.so ./gm
+export LD_PRELOAD="$PWD/../libmirror.so"
