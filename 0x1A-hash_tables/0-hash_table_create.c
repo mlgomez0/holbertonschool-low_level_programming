6f48f94157de2a38951ddce8f02d1034c8fed35c
mlgomez0 <mlgomez230@hotmail.com>
2020-01-18 22:22:34 +0000

    0- negative #

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,14 +12,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **new_array;
 	unsigned long int i;
 
-	if (size <= 0)
-		return (NULL);
 	newh_table = malloc(sizeof(hash_table_t));
 	if (newh_table == NULL)
 		return (NULL);
 	new_array = malloc(sizeof(hash_node_t *) * size);
 	if (new_array == NULL)
+	{
+		free(newh_table);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		new_array[i] = NULL;
--- 0x1A-hash_tables/6-main.c
+++ 0x1A-hash_tables/6-main.c
@@ -11,18 +11,19 @@
 int main(void)
 {
     hash_table_t *ht = NULL;
-   /* char *key;
+    /*char *key;
     char *value;*/
 
-    /*ht = hash_table_create(1024);*/
-    hash_table_set(ht, "c", "fun");
-    hash_table_set(ht, "python", "awesome");
+    ht = hash_table_create(-1024);
+    hash_table_set(ht, "hola", "fun");
+
+   /* hash_table_set(ht, "python", "awesome");
     hash_table_set(ht, "Jennie", "and Jay love asm");
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Holberton");
     hash_table_set(ht, "98", "Battery Streetz");
-   /* key = strdup("Tim");
+    key = strdup("Tim");
     value = strdup("Britton");
     hash_table_set(ht, key, value);
     key[0] = '\0';
@@ -33,8 +34,8 @@ int main(void)
     hash_table_set(ht, "hetairas", "Jennie");
     hash_table_set(ht, "hetairas", "Jennie Z");
     hash_table_set(ht, "mentioner", "Jennie");
-    hash_table_set(ht, "hetairas", "Jennie Z Chu");
+    hash_table_set(ht, "hetairas", "Jennie Z Chu");*/
     hash_table_print(ht);
-    hash_table_delete(ht);*/
+    hash_table_delete(ht);
     return (EXIT_SUCCESS);
 }
