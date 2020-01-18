f782313b3f8fc6af8673d29926d07b14f0d77020
mlgomez0 <mlgomez230@hotmail.com>
2020-01-18 20:22:53 +0000

    second commit 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,7 +18,7 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			temp = ht->array[i];
 			if (temp != NULL)
-				free_list(temp);
+				free_list_h(temp);
 		}
 		free(ht->array);
 		free(ht);
@@ -26,11 +26,11 @@ void hash_table_delete(hash_table_t *ht)
 }
 
 /**
- *free_list - free the memory space in a linked list
+ *free_list_h - free the memory space in a linked list
  *@head: head of the linked list
  */
 
-void free_list(hash_node_t *head)
+void free_list_h(hash_node_t *head)
 {
 	hash_node_t *temp;
 
