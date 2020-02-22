39f57fb71aa28899c69a24b0a45e62885bbdc30a
mlgomez0 <mlgomez230@hotmail.com>
2020-02-22 01:46:23 +0000

    correction: 5 - Perimeter formula

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,11 +7,11 @@ def island_perimeter(grid):
     """returns the perimeter of the island described in grid
     """
     count = 0
-    for i in grid:
-        for j in i:
-            if j == 1:
-                count = count + 1
-    if count != 0:
-        count = count + 1
-    count = count * 2
-    return count
+    if (len(grid) <= 100 and len(grid[0]) <= 100):
+        for i in grid:
+            for j in i:
+                if j == 1:
+                    count = count + 1
+        if count != 0:
+            count = count * 2 + 2
+        return count
